e3dabf7200794404b2339f3f32502f8d63d81c64
SeaNvrsk <seanvrsk@gmail.com>
2022-06-02 18:38:30 +0300

    first

---
+++ shop/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Product, StyleOfBeer
+# Register your models here.
+
+
+admin.site.register(StyleOfBeer)
+admin.site.register(Product)
---
+++ shop/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class StyleOfBeer(models.Model):
+    class Meta:
+        verbose_name = 'Тип пива'
+        verbose_name_plural = 'Типы пива'
+
+    TYPE_CHOICES = (
+        ('ALE', 'Эль'),
+        ('LAGER', 'Лагер')
+    )
+
+    title = models.CharField(max_length=200, verbose_name="Тип пива")
+    typeOfBeer = models.CharField(max_length=10, choices=TYPE_CHOICES, verbose_name="Вид пива")
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    CONTAINER_CHOICES = (
+        ('BOTTLE', 'бутылка'),
+        ('CAN', 'банка'),
+    )
+
+    name = models.CharField(max_length=200, verbose_name="Название")
+    styleOfBeer = models.ForeignKey(StyleOfBeer, on_delete=models.CASCADE, verbose_name="Тип пива")
+    container = models.CharField(max_length=6, choices=CONTAINER_CHOICES, verbose_name="Тара")
+    size = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Объём")
+    volume = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Крепость")
+    beerDensity = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Плотность пива")
+    description = models.CharField(max_length=500, verbose_name="Описание")
+    manufacturer = models.CharField(max_length=100, verbose_name="Производитель")
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    inStock = models.BooleanField(default=True)
+    price = models.PositiveIntegerField(verbose_name="цена")
+    quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
+
+    def __str__(self):
+        return self.name
+
+
